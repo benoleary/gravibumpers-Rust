a94c831c707e646191f24085cc74179ce42a91ac
Ben O'Leary <benjamin.oleary@gmail.com>
2020-06-04 19:27:35 +0100

    Begin implementation of creating circle of particles from JSON configuration (does not complile because I have not implemented the velocity part yet).

--- gravibumpers/initial_conditions/src/circle.rs
+++ gravibumpers/initial_conditions/src/circle.rs
@@ -1,39 +1,163 @@
 use super::ConfigurationParseError;
+use std::convert::TryInto;
 
 const RADIUS_LABEL: &str = "radius";
 const POPULATION_LABEL: &str = "population";
+const ROTATION_LABEL: &str = "rotation";
+const MASS_LABEL: &str = "mass";
+const GRAV_LABEL: &str = "grav";
+const BUMP_LABEL: &str = "bump";
+const RED_LABEL: &str = "red";
+const GREEN_LABEL: &str = "green";
+const BLUE_LABEL: &str = "blue";
 
 pub fn from_json(
     given_configuration: &serde_json::Value,
 ) -> Result<Box<dyn data_structure::ParticleIteratorProvider>, Box<dyn std::error::Error>> {
-    let circle_radius = match given_configuration[RADIUS_LABEL].as_f64() {
-        Some(parsed_number) => parsed_number,
-        _ => {
-            return Err(Box::new(ConfigurationParseError::new(&format!(
-                "Could not parse \"{}\" from {}",
-                RADIUS_LABEL, given_configuration
-            ))))
-        }
+    let circle_radius = parse_f64(RADIUS_LABEL, given_configuration)?;
+    let circle_population = parse_i64(POPULATION_LABEL, given_configuration)?;
+    let circle_rotation = parse_f64(ROTATION_LABEL, given_configuration)?;
+    let inertial_mass = parse_f64(MASS_LABEL, given_configuration)?;
+    let attractive_charge = parse_f64(GRAV_LABEL, given_configuration)?;
+    let repulsive_charge = parse_f64(BUMP_LABEL, given_configuration)?;
+    let red_brightness = parse_f64(RED_LABEL, given_configuration)?;
+    let green_brightness = parse_f64(GREEN_LABEL, given_configuration)?;
+    let blue_brightness = parse_f64(BLUE_LABEL, given_configuration)?;
+    let common_intrinsics = data_structure::ParticleIntrinsics {
+        inertial_mass: data_structure::InertialMassUnit(inertial_mass),
+        attractive_charge: data_structure::AttractiveChargeUnit(attractive_charge),
+        repulsive_charge: data_structure::RepulsiveChargeUnit(repulsive_charge),
+        red_brightness: data_structure::RedColorUnit(red_brightness),
+        green_brightness: data_structure::GreenColorUnit(green_brightness),
+        blue_brightness: data_structure::BlueColorUnit(blue_brightness),
     };
-    let circle_population = match given_configuration[POPULATION_LABEL].as_i64() {
-        Some(parsed_number) => parsed_number,
-        _ => {
-            return Err(Box::new(ConfigurationParseError::new(&format!(
-                "Could not parse \"{}\" from {}",
-                POPULATION_LABEL, given_configuration
-            ))))
-        }
-    };
-    from_numbers(circle_radius, circle_population)
+    particles_from_numbers(
+        circle_radius,
+        circle_population,
+        circle_rotation,
+        common_intrinsics,
+    )
 }
 
-fn from_numbers(
+pub fn parse_f64(
+    attribute_label: &str,
+    given_configuration: &serde_json::Value,
+) -> Result<f64, Box<dyn std::error::Error>> {
+    match given_configuration[attribute_label].as_f64() {
+        Some(parsed_number) => Ok(parsed_number),
+        _ => Err(Box::new(ConfigurationParseError::new(&format!(
+            "Could not parse \"{}\" from {}",
+            attribute_label, given_configuration
+        )))),
+    }
+}
+
+pub fn parse_i64(
+    attribute_label: &str,
+    given_configuration: &serde_json::Value,
+) -> Result<i64, Box<dyn std::error::Error>> {
+    match given_configuration[attribute_label].as_i64() {
+        Some(parsed_number) => Ok(parsed_number),
+        _ => Err(Box::new(ConfigurationParseError::new(&format!(
+            "Could not parse \"{}\" from {}",
+            attribute_label, given_configuration
+        )))),
+    }
+}
+
+fn particles_from_numbers(
     circle_radius: f64,
     circle_population: i64,
+    circle_rotation: f64,
+    common_intrinsics: data_structure::ParticleIntrinsics,
 ) -> Result<Box<dyn data_structure::ParticleIteratorProvider>, Box<dyn std::error::Error>> {
-    Err(Box::new(ConfigurationParseError::new(&format!(
-        "Not yet implemented"
-    ))))
+    if circle_population < 2 {
+        return Err(Box::new(ConfigurationParseError::new(&format!(
+            "Population {} is not large enough (must be 2 or larger)",
+            circle_population
+        ))));
+    }
+
+    let mut circle_particles: std::vec::Vec<data_structure::IndividualParticle> =
+        std::vec::Vec::with_capacity(circle_population.try_into()?);
+
+    // We always start with a particle at north, the top of the circle.
+    circle_particles.push(data_structure::IndividualParticle {
+        intrinsic_values: common_intrinsics,
+        variable_values: data_structure::ParticleVariables {
+            horizontal_position: data_structure::HorizontalPositionUnit(0.0),
+            vertical_position: data_structure::VerticalPositionUnit(circle_radius),
+            horizontal_velocity: data_structure::HorizontalVelocityUnit(
+                circle_radius * circle_rotation,
+            ),
+            vertical_velocity: data_structure::VerticalVelocityUnit(0.0),
+        },
+    });
+    if (circle_population % 2) == 0 {
+        // If the number of particles is even, then there is a particle at south, the bottom of the
+        // circle.
+        circle_particles.push(data_structure::IndividualParticle {
+            intrinsic_values: common_intrinsics,
+            variable_values: data_structure::ParticleVariables {
+                horizontal_position: data_structure::HorizontalPositionUnit(0.0),
+                vertical_position: data_structure::VerticalPositionUnit(-circle_radius),
+                horizontal_velocity: data_structure::HorizontalVelocityUnit(
+                    -circle_radius * circle_rotation,
+                ),
+                vertical_velocity: data_structure::VerticalVelocityUnit(0.0),
+            },
+        });
+    }
+
+    // Apart from the particle at north, and a possible particle at south, the particles come
+    // in pairs reflected in the vertical axis.
+    let number_of_horizontal_pairs = (circle_population / 2) - 1;
+
+    if number_of_horizontal_pairs > 0 {
+        let angle_between_particles_in_radians =
+            std::f64::consts::PI / (number_of_horizontal_pairs + 1) as f64;
+        let mut angle_from_north_in_radians = 0.0;
+        for _ in 1..number_of_horizontal_pairs {
+            angle_from_north_in_radians += angle_between_particles_in_radians;
+            let horizontal_position_value = angle_from_north_in_radians.sin() * circle_radius;
+            let vertical_position_value = angle_from_north_in_radians.cos() * circle_radius;
+
+            circle_particles.push(data_structure::IndividualParticle {
+                intrinsic_values: common_intrinsics,
+                variable_values: data_structure::ParticleVariables {
+                    horizontal_position: data_structure::HorizontalPositionUnit(
+                        horizontal_position_value,
+                    ),
+                    vertical_position: data_structure::VerticalPositionUnit(
+                        vertical_position_value,
+                    ),
+                    horizontal_velocity: data_structure::HorizontalVelocityUnit(??),
+                    vertical_velocity: data_structure::VerticalVelocityUnit(??),
+                },
+            });
+        }
+    }
+
+    Ok(data_structure::wrap_particle_vector(circle_particles))
+}
+
+fn particle_from_numbers(
+    circle_radius: f64,
+    angle_from_north_in_radians: f64,
+    circle_rotation: f64,
+    common_intrinsics: data_structure::ParticleIntrinsics,
+) -> data_structure::IndividualParticle {
+    data_structure::IndividualParticle {
+        intrinsic_values: common_intrinsics,
+        variable_values: data_structure::ParticleVariables {
+            horizontal_position: data_structure::HorizontalPositionUnit(0.0),
+            vertical_position: data_structure::VerticalPositionUnit(circle_radius),
+            horizontal_velocity: data_structure::HorizontalVelocityUnit(
+                circle_radius * circle_rotation,
+            ),
+            vertical_velocity: data_structure::VerticalVelocityUnit(0.0),
+        },
+    }
 }
 
 #[cfg(test)]
